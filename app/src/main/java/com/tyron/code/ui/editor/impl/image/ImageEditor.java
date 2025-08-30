eb4e1d9acdafcb69d1adb1c9c9e09a3e4a09b398
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2025-08-30 12:44:44 +0330

    Implement SAF Uri support in ImageEditor
    Added support for SAF Uri in ImageEditor class.

--- app/src/main/java/com/tyron/code/ui/editor/impl/image/ImageEditor.java
+++ app/src/main/java/com/tyron/code/ui/editor/impl/image/ImageEditor.java
@@ -1,5 +1,6 @@
 package com.tyron.code.ui.editor.impl.image;
 
+import android.net.Uri;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -10,35 +11,57 @@ import com.tyron.fileeditor.api.FileEditor;
 import java.io.File;
 import java.util.Objects;
 
+/**
+ * ImageEditor implementation supporting both local File and SAF Uri.
+ */
 public class ImageEditor implements FileEditor {
 
     private final File mFile;
+    private final Uri mUri;
     private final ImageEditorProvider mProvider;
     private ImageEditorFragment mFragment;
 
+    // Constructor for local File
     public ImageEditor(@NonNull File file, ImageEditorProvider provider) {
         mFile = file;
+        mUri = null;
         mProvider = provider;
-        mFragment = createFragment(file);
+        mFragment = createFragment(file, null);
     }
 
-    protected ImageEditorFragment createFragment(@NonNull File file) {
-        return ImageEditorFragment.newInstance(file);
+    // Constructor for SAF Uri
+    public ImageEditor(@NonNull Uri uri, ImageEditorProvider provider) {
+        mFile = null;
+        mUri = uri;
+        mProvider = provider;
+        mFragment = createFragment(null, uri);
     }
 
-//    @Override
-//    public Fragment getFragment() {
-//        return mFragment;
-//    }
+    protected ImageEditorFragment createFragment(File file, Uri uri) {
+        if (file != null) {
+            return ImageEditorFragment.newInstance(file);
+        }
+        if (uri != null) {
+            return ImageEditorFragment.newInstance(uri);
+        }
+        throw new IllegalArgumentException("Both file and uri are null");
+    }
+
+    @Override
+    public Fragment getFragment() {
+        return mFragment;
+    }
 
     @Override
     public View getView() {
-        return null;
+        // If you have a custom view inside fragment and want to expose it
+        return mFragment != null ? mFragment.getView() : null;
     }
 
     @Override
     public View getPreferredFocusedView() {
-        return mFragment.getView();
+        // Focus on fragment's view if exists
+        return mFragment != null ? mFragment.getView() : null;
     }
 
     @NonNull
@@ -49,12 +72,14 @@ public class ImageEditor implements FileEditor {
 
     @Override
     public boolean isModified() {
+        // For images, typically editing is not supported, so always false
         return false;
     }
 
     @Override
     public boolean isValid() {
-        return true;
+        // Valid if either file or uri is not null
+        return mFile != null || mUri != null;
     }
 
     @Override
@@ -62,16 +87,22 @@ public class ImageEditor implements FileEditor {
         return mFile;
     }
 
+    // SAF support: getUri if using SAF
+    public Uri getUri() {
+        return mUri;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ImageEditor that = (ImageEditor) o;
-        return Objects.equals(mFile, that.mFile);
+        // Compare file or uri depending on usage
+        return Objects.equals(mFile, that.mFile) && Objects.equals(mUri, that.mUri);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(mFile);
+        return Objects.hash(mFile, mUri);
     }
 }
