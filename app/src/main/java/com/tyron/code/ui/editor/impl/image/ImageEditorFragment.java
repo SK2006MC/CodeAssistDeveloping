330e80762e233d46f88610eda5f33e06c91c41e2
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2025-08-30 12:46:16 +0330

    Enhance ImageEditorFragment to handle Uri images
    Added support for loading images from Uri in ImageEditorFragment.

--- app/src/main/java/com/tyron/code/ui/editor/impl/image/ImageEditorFragment.java
+++ app/src/main/java/com/tyron/code/ui/editor/impl/image/ImageEditorFragment.java
@@ -1,5 +1,6 @@
 package com.tyron.code.ui.editor.impl.image;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,9 @@ import com.tyron.code.R;
 
 import java.io.File;
 
+/**
+ * ImageEditorFragment supporting both local File and SAF Uri images.
+ */
 public class ImageEditorFragment extends Fragment {
 
     public static ImageEditorFragment newInstance(File file) {
@@ -25,20 +29,45 @@ public class ImageEditorFragment extends Fragment {
         return fragment;
     }
 
+    public static ImageEditorFragment newInstance(Uri uri) {
+        ImageEditorFragment fragment = new ImageEditorFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("uri", uri);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         ImageView imageView = new ImageView(requireContext());
-        if (getArguments() != null) {
-            String file = requireArguments().getString("file", "");
-            File imageFile = new File(file);
-            if (imageFile.exists()) {
-                Glide.with(imageView)
-                        .load(imageFile)
-                        .into(imageView);
+        Bundle args = getArguments();
+        if (args != null) {
+            if (args.containsKey("file")) {
+                String file = args.getString("file", "");
+                File imageFile = new File(file);
+                if (imageFile.exists()) {
+                    Glide.with(imageView)
+                            .load(imageFile)
+                            .placeholder(R.drawable.ic_image_placeholder)
+                            .error(R.drawable.ic_image_error)
+                            .into(imageView);
+                }
+            } else if (args.containsKey("uri")) {
+                Uri uri = args.getParcelable("uri");
+                if (uri != null) {
+                    Glide.with(imageView)
+                            .load(uri)
+                            .placeholder(R.drawable.ic_image_placeholder)
+                            .error(R.drawable.ic_image_error)
+                            .into(imageView);
+                }
             }
         }
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
         return imageView;
     }
 }
